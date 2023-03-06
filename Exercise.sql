95c6085c487d902206013b60f25bdf75ab81796c
Michael Yapi .A <michaelakichi@gmail.com>
2023-03-06 03:20:24 +0000

    Update Exercise.sql

--- Exercise.sql
+++ Exercise.sql
@@ -50,5 +50,5 @@ SELECT first_name, last_name FROM customers ORDER BY last_name LIMIT 4;
 SELECT first_name, last_name, birth_date FROM customers ORDER BY birth_date LIMIT 1;
 
 --Fetch three students skipping the first two students:
-SELECT first_name, last_name FROM customers ORDER BY last_name OFFSET 2 LIMIT 3;
+SELECT first_name, last_name FROM customers ORDER BY last_name OFFSET 2 LIMIT 3; 
 
